f79ba96425bab2825ebe002adcd41ec83320c8d3
Nachiket <nachiketapte14@gmail.com>
2023-07-20 18:40:16 +0530

    Modifications!!!

--- src/Sub_Project_01/Closeness_Centrality1.java
+++ src/Sub_Project_01/Closeness_Centrality1.java
@@ -4,7 +4,7 @@ import edu.princeton.cs.algs4.DijkstraUndirectedSP;
 import edu.princeton.cs.algs4.EdgeWeightedGraph;
 import edu.princeton.cs.algs4.In;
 
-public class PlotTables {
+public class Closeness_Centrality1 {
     public static void main(String[] args) {
         In in1 = new In("src/Sub_Project_01/Datasets/Graph_01.txt");
         In in2 = new In("src/Sub_Project_01/Datasets/Graph_02.txt");
@@ -59,9 +59,23 @@ public class PlotTables {
         float avg_avg = (avg_closeness_centrality1 + avg_closeness_centrality2 + avg_closeness_centrality3) / 3.0f;
         float minimum = Math.min(avg_closeness_centrality3,Math.min(avg_closeness_centrality1,avg_closeness_centrality2));
         float maximum = Math.max(avg_closeness_centrality3,Math.max(avg_closeness_centrality1,avg_closeness_centrality2));
+        String max_transport = "", min_transport = "";
+        if(maximum == avg_closeness_centrality1)
+                max_transport = "Roadways";
+        else if(maximum == avg_closeness_centrality2)
+                max_transport = "Railways";
+        else
+                max_transport = "Subways";
+        if(minimum == avg_closeness_centrality1)
+            min_transport = "Roadways";
+        else if(minimum == avg_closeness_centrality2)
+            min_transport = "Railways";
+        else
+            min_transport = "Subways";
         System.out.println("Average of all : " + avg_avg);
-        System.out.println("Min of all : " + minimum);
-        System.out.println("Max of all: " + maximum);
+        System.out.println("Min of all : " + minimum + "("+min_transport + ")");
+        System.out.println("Max of all: " + maximum + "("+max_transport + ")");
+
         float median  = (avg_closeness_centrality1 + avg_closeness_centrality2 + avg_closeness_centrality3) - maximum - minimum;
         System.out.println("Median of all : " + median);
     }
--- src/Sub_Project_02/Closeness_Centrality2.java
+++ src/Sub_Project_02/Closeness_Centrality2.java
@@ -1,11 +1,11 @@
 package Sub_Project_02;
 import edu.princeton.cs.algs4.*;
 
-public class PlotTables_2 {
+public class Closeness_Centrality2 {
     public static void main(String[] args) {
-        In in1 = new In("01_Government.txt");
-        In in2 = new In("02_Politicians.txt");
-        In in3 = new In("03_Public_figure.txt");
+        In in1 = new In("src/Sub_Project_02/01_Government.txt");
+        In in2 = new In("src/Sub_project_02/02_Politicians.txt");
+        In in3 = new In("src/Sub_project_02/03_Public_figure.txt");
         EdgeWeightedGraph g1 = new EdgeWeightedGraph(in1);
         EdgeWeightedGraph g2 = new EdgeWeightedGraph(in2);
         EdgeWeightedGraph g3 = new EdgeWeightedGraph(in3);
